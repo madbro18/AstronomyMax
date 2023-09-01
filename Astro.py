3535c414c0f802553f51487a18b61625b87c9662
madbro18 <143666548+madbro18@users.noreply.github.com>
2023-09-01 17:56:54 +0300

    Astro.py

--- Astro.py
+++ Astro.py
@@ -293,7 +293,9 @@ with image_column:
     st.image(Mercury_in_true_color)
     with text_column:
      st.write("------")
-     st.subheader("Mercury")
+        st.subheader("Mercury")
+         st.write("------")
+     st.subheader("Venus")
     st.write(
     """ Mercury is the first planet from the Sun and the smallest planet in the Solar System. It is a terrestrial planet with a heavily cratered surface due to the planet having no geological activity and an extremely tenuous atmosphere (called an exosphere).
      Despite being the smallest planet in the Solar System with a mean diameter of 4,880 km (3,030 mi), 38% of that of Earth's, Mercury is dense enough to have roughly the same surface gravity as Mars. Mercury has a dynamic magnetic field with a strength about 1% of that of Earth's and has no natural satellites.
@@ -346,7 +348,6 @@ with image_column:
         st.image(venus2_pic)
         with text_column:
             st.write("------")
-            st.subheader("Venus")
 
         st.write(
                    """
